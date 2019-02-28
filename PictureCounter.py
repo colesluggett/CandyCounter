3589c0c7e7f1681e15c01a356dec2ca5fec36a07
kaylaawheelerr <kaylaawheelerr@gmail.com>
2019-02-28 12:43:57 -0700

    Update PictureCounter.py

--- PictureCounter.py
+++ PictureCounter.py
@@ -1,14 +1,14 @@
 import numpy as np
 import cv2 as cv
 
-img = cv.imread("four.jpg", cv.IMREAD_COLOR)
+img = cv.imread("one.jpg", cv.IMREAD_COLOR)
 
 cv.imshow("img", img)
 
 param = cv.SimpleBlobDetector_Params()
 
 blur = cv.blur(img,(3,3))
-cv.imshow('Blur', blur)
+#cv.imshow('Blur', blur)
 
 
 kernel = np.ones((4,4), np.uint8)
@@ -17,34 +17,46 @@ erosion = cv.erode(blur, kernel)
 kernel = np.ones((4,4), np.uint8)
 dilation = cv.dilate(erosion, kernel)
 
-cv.imshow('Erosion', erosion)
-cv.imshow('Dilation',dilation)
+#cv.imshow('Erosion', erosion)
+#cv.imshow('Dilation',dilation)
 
 edges = cv.Canny(dilation, 10, 300)
-cv.imshow('Edge', edges)
+#cv.imshow('Edge', edges)
 thresh = edges.copy()
 
 kernel = np.ones((3,3), np.uint8)
 edge_dilat= cv.dilate(edges, kernel)
-cv.imshow('edge dialtion', edge_dilat)
+#cv.imshow('edge dialtion', edge_dilat)
 
-circles = cv.HoughCircles(edge_dilat, cv.HOUGH_GRADIENT, 1.2, 100)
-output = img.copy() 
-# ensure at least some circles were found
-if circles is not None:
-	# convert the (x, y) coordinates and radius of the circles to integers
-	circles = np.round(circles[0, :]).astype("int")
- 
-	# loop over the (x, y) coordinates and radius of the circles
-	for (x, y, r) in circles:
-		# draw the circle in the output image, then draw a rectangle
-		# corresponding to the center of the circle
-		cv.circle(output, (x, y), r, (0, 255, 0), 4)
-		cv.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
- 
-	# show the output image
-	cv.imshow("output", np.hstack([img, output]))
+circles = cv.HoughCircles(edge_dilat,cv.HOUGH_GRADIENT,1,20,param1=50,param2=25,minRadius=10,maxRadius=40)
 
+circles = np.uint16(np.around(circles))
+listOfCircleImages = [] 
+for i in circles[0,:]:
+    currentImg = np.zeros((img.shape[0],img.shape[1]), np.uint8) 
+    cv.circle(currentImg,(i[0],i[1]),i[2],(255,255,255), -1) 
+    cv.circle(edge_dilat,(i[0],i[1]),i[2],(255,255,255), -1) 
+    listOfCircleImages.append(currentImg)
+    
+for img in listOfCircleImages:
+    rgb_data = cv.mean(img, mask=img)
+
+
+
+font = cv.FONT_HERSHEY_SIMPLEX
+cv.putText(img,'Yellow Red Blue Green Brown Orange', (0,0), font, 1, (200,255,255), 2, cv.LINE_AA)
+
+cv.imshow('Detected Circles',edge_dilat)
+
+'''
+M&M's  rgb color
+Green 49 172 215
+Yellow 255 242 0
+Orange 242 111 34
+Brown 96 58 52
+Red 177 18 36
+Blue 47 159 215
+'''
 
 cv.waitKey(0)
 cv.destroyAllWindows()
